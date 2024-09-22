7c140e4db2a15d2b3fc990fab52291dc542fe311
PeddarmajiSaikumar <saikumarpeddarmaji@gmail.com>
2024-09-22 15:10:32 +0530

    Changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { ExploreComponent } from './explore/explore.component';
+import { HomeComponent } from './home/home.component';
+import { OffersComponent } from './offers/offers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'home',component:HomeComponent},
+    {path:'explore',component:ExploreComponent},
+    {path:'offers',component:OffersComponent},
+    {path:'events',component:EventsComponent}
+];
